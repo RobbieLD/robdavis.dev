dc6fed7b5a711f1d810ebaa38fe529110722ced5
Rob Davis <bfe5e925@opayq.com>
2022-06-02 22:07:58 +1000

    Reference formatting

--- ui.robdavis.dev/src/components/ReferenceList.vue
+++ ui.robdavis.dev/src/components/ReferenceList.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="references">
         <div class="ref" v-for="(ref, index) of refs" :key="index">
-            <Chip :label="'<b>' + ref.Name + '</b>'" :image="ref.Avatar.url"/>
+            <div class="ref__right">
+                <div class="ref__name">{{ ref.Name }}</div>
+                <div class="ref__job">{{ ref.Role }} at {{ ref.Company }}</div>
+                <div class="ref__email-row">
+                    <font-awesome-icon icon="envelope" class="ref__icon" />
+                    <div class="ref__email">
+                        <a target="_blank" :href="'mailto:' + ref.Email">{{ ref.Email }}</a>
+                    </div>
+                </div>
+                <div class="ref__phone-row">
+                    <font-awesome-icon icon="square-phone-flip" class="ref__icon" />
+                    <div class="ref__phone">{{ ref.Phone }}</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -10,13 +23,10 @@
     import { storeKey } from '@/store'
     import { computed, defineComponent } from 'vue'
     import { useStore } from 'vuex'
-    import Chip from 'primevue/chip'
 
     export default defineComponent({
         name: 'ReferenceList',
-        components: {
-            Chip
-        },
+        components: {},
         setup() {
             const store = useStore(storeKey)
             const refs = computed(() => store.state.references)
@@ -29,6 +39,56 @@
 </script>
 <style lang="scss">
     .references {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 0.5em;
+    }
 
+    .ref {
+        margin-bottom: 1em;
+        margin-right: 1em;
+        display: flex;
+        
+
+        &__icon {
+            align-self: end;
+            padding-right: 0.2em;
+            color: var(--secondary-color);
+            font-size: 1.1em;
+        }
+
+        &__phone {
+            &-row {
+                display: flex;
+            }
+        }
+
+        &__email {
+            a {
+                text-decoration: none;
+                color: black;
+
+                &:hover {
+                    color:var(--secondary-color)
+                }
+            }
+
+            &-row {
+                display: flex;
+            }
+        }
+        
+        &__name {
+            font-weight: 500;
+            color: var(--primary-color);
+            font-size: 1.1em;
+            border-bottom: dashed 1px var(--secondary-color);
+        }
+
+        &__job {
+            color: var(--light-grey);
+            font-variant: all-small-caps;
+        }
     }
 </style>
